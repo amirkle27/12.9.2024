f727beb3e9a935646bae1b0e265aed2ad256f5ab
amirkle27 <amirkle@gmail.com>
2024-09-14 18:58:53 +0300

    Update Homework no.2.py

--- Homework no.2.py
+++ Homework no.2.py
@@ -1,28 +1,34 @@
 from fileinput import close
 
 from numpy.ma.extras import average
-
+from contextlib import contextmanager
 import sqlite_lib
+
+@contextmanager
+def db_connection():
+    sqlite_lib.connect('ecomm.db')
+    try:
+        yield
+    finally:
+        sqlite_lib.close()
+
 
 #2
 #a
 def count_customers ():
-    sqlite_lib.connect('ecomm.db')
-    customers_number = sqlite_lib.run_query_select("SELECT COUNT ('Customer ID')"
-                                                   "FROM 'E-commerce Customer Behavior - Sheet1'")
-    sqlite_lib.close()
+    with db_connection():
+                customers_number = sqlite_lib.run_query_select("SELECT COUNT ('Customer ID')"
+                                                       "FROM 'E-commerce Customer Behavior - Sheet1'")
     return customers_number [0][0]
-
 
 if __name__ == "__main__":
     print(count_customers())
 
 #b
 def average_customer_age ():
-    sqlite_lib.connect('ecomm.db')
-    average_age = sqlite_lib.run_query_select("SELECT AVG(Age)"
-                                              "FROM 'E-commerce Customer Behavior - Sheet1'")
-    sqlite_lib.close()
+    with db_connection():
+        average_age = sqlite_lib.run_query_select("SELECT AVG(Age)"
+                                                  "FROM 'E-commerce Customer Behavior - Sheet1'")
     return average_age[0][0]
 
 if __name__ == "__main__":
@@ -30,17 +36,16 @@ if __name__ == "__main__":
 
 #c
 def counting_men_women ():
-    sqlite_lib.connect('ecomm.db')
-    counting_men = sqlite_lib.run_query_select("SELECT COUNT (*)"
-                                               "FROM 'E-commerce Customer Behavior - Sheet1'"
-                                               "WHERE Gender = 'Male'")
+    with db_connection():
+        counting_men = sqlite_lib.run_query_select("SELECT COUNT (*)"
+                                                   "FROM 'E-commerce Customer Behavior - Sheet1'"
+                                                   "WHERE Gender = 'Male'")
 
-    counting_women = sqlite_lib.run_query_select("SELECT COUNT (*)"
-                                                 "FROM 'E-commerce Customer Behavior - Sheet1'"
-                                                 "WHERE GENDER = 'Female'")
+        counting_women = sqlite_lib.run_query_select("SELECT COUNT (*)"
+                                                     "FROM 'E-commerce Customer Behavior - Sheet1'"
+                                                     "WHERE GENDER = 'Female'")
 
-    answer = (f"Number of Male Customers: {counting_men[0][0]}\nNumber of Female Customers: {counting_women[0][0]}")
-    sqlite_lib.close()
+        answer = (f"Number of Male Customers: {counting_men[0][0]}\nNumber of Female Customers: {counting_women[0][0]}")
     return answer
 
 if __name__ == "__main__":
@@ -48,17 +53,16 @@ if __name__ == "__main__":
 
 #d
 def average_purchases ():
-    sqlite_lib.connect('ecomm.db')
-    men_purchase_average = sqlite_lib.run_query_select('SELECT AVG("Items Purchased")'
-                                                       'FROM "E-commerce Customer Behavior - Sheet1"'
-                                                       'WHERE Gender = "Male"')
+    with db_connection():
+        men_purchase_average = sqlite_lib.run_query_select('SELECT AVG("Items Purchased")'
+                                                           'FROM "E-commerce Customer Behavior - Sheet1"'
+                                                           'WHERE Gender = "Male"')
 
-    women_purchase_average = sqlite_lib.run_query_select('SELECT AVG("Items Purchased")'
-                                                         'FROM "E-commerce Customer Behavior - Sheet1"'
-                                                         'WHERE Gender = "Female"')
+        women_purchase_average = sqlite_lib.run_query_select('SELECT AVG("Items Purchased")'
+                                                             'FROM "E-commerce Customer Behavior - Sheet1"'
+                                                             'WHERE Gender = "Female"')
 
-    answer = (f"Average Purchases made by Male Customers: {men_purchase_average[0][0]}\nAverage Purchases made by Female Customers: {women_purchase_average[0][0]}")
-    sqlite_lib.close()
+        answer = (f"Average Purchases made by Male Customers: {men_purchase_average[0][0]}\nAverage Purchases made by Female Customers: {women_purchase_average[0][0]}")
     return answer
 
 if __name__ == "__main__":
@@ -66,10 +70,9 @@ if __name__ == "__main__":
 
 #e
 def membership_types ():
-    sqlite_lib.connect('ecomm.db')
-    memberships = sqlite_lib.run_query_select('SELECT COUNT (DISTINCT "Membership Type")'
-                                              'FROM "E-commerce Customer Behavior - Sheet1"')
-    sqlite_lib.close()
+    with db_connection():
+        memberships = sqlite_lib.run_query_select('SELECT COUNT (DISTINCT "Membership Type")'
+                                                  'FROM "E-commerce Customer Behavior - Sheet1"')
     return memberships [0][0]
 
 if __name__ == "__main__":
@@ -77,11 +80,10 @@ if __name__ == "__main__":
 
 #f
 def counting_members_by_membership_kinds ():
-    sqlite_lib.connect('ecomm.db')
-    count = sqlite_lib.run_query_select('SELECT "Membership Type", COUNT ("Customer ID")'
-                                        'FROM "E-commerce Customer Behavior - Sheet1"'
-                                        'GROUP BY "Membership Type"')
-    sqlite_lib.close()
+    with db_connection():
+        count = sqlite_lib.run_query_select('SELECT "Membership Type", COUNT ("Customer ID")'
+                                            'FROM "E-commerce Customer Behavior - Sheet1"'
+                                            'GROUP BY "Membership Type"')
     return count
 
 if __name__ == "__main__":
@@ -91,11 +93,10 @@ if __name__ == "__main__":
 
 #g
 def counting_ny_customets ():
-    sqlite_lib.connect('ecomm.db')
-    counting = sqlite_lib.run_query_select('SELECT "City", COUNT ("Customer ID")'
-                                           ' FROM "E-commerce Customer Behavior - Sheet1"'
-                                           ' Where City = "New York"')
-    sqlite_lib.close()
+    with db_connection():
+        counting = sqlite_lib.run_query_select('SELECT "City", COUNT ("Customer ID")'
+                                               ' FROM "E-commerce Customer Behavior - Sheet1"'
+                                               ' Where City = "New York"')
     return counting
 
 if __name__ == "__main__":
@@ -104,12 +105,11 @@ if __name__ == "__main__":
 
 #h
 def counting_customers_by_city ():
-    sqlite_lib.connect('ecomm.db')
-    count = sqlite_lib.run_query_select('SELECT "City", COUNT("Customer ID")'
-                                        'FROM "E-commerce Customer Behavior - Sheet1"'
-                                        'GROUP BY "City"'
-                                        'ORDER BY "Customer ID" ' )
-    sqlite_lib.close()
+    with db_connection():
+        count = sqlite_lib.run_query_select('SELECT "City", COUNT("Customer ID")'
+                                            'FROM "E-commerce Customer Behavior - Sheet1"'
+                                            'GROUP BY "City"'
+                                            'ORDER BY "Customer ID" ' )
     return count
 
 if __name__ == "__main__":
@@ -118,11 +118,10 @@ if __name__ == "__main__":
 
 #i
 def total_spend_men_women ():
-    sqlite_lib.connect('ecomm.db')
-    count = sqlite_lib.run_query_select('SELECT "Gender", SUM("TOTAL SPEND")'
-                                        'FROM "E-commerce Customer Behavior - Sheet1"'
-                                        'GROUP BY "Gender"')
-    sqlite_lib.close()
+    with db_connection():
+        count = sqlite_lib.run_query_select('SELECT "Gender", SUM("TOTAL SPEND")'
+                                            'FROM "E-commerce Customer Behavior - Sheet1"'
+                                            'GROUP BY "Gender"')
     return count
 
 if __name__ == "__main__":
@@ -131,25 +130,23 @@ if __name__ == "__main__":
 
 #j
 def most_least_products ():
-    sqlite_lib.connect('ecomm.db')
-    count_max = sqlite_lib.run_query_select(
-        'SELECT "Customer ID", "Items Purchased" '
-        'FROM "E-commerce Customer Behavior - Sheet1" '
-        'WHERE "Items Purchased" = (SELECT MAX("Items Purchased")'
-        'FROM "E-commerce Customer Behavior - Sheet1")'
-    )
+    with db_connection():
+        count_max = sqlite_lib.run_query_select(
+            'SELECT "Customer ID", "Items Purchased" '
+            'FROM "E-commerce Customer Behavior - Sheet1" '
+            'WHERE "Items Purchased" = (SELECT MAX("Items Purchased")'
+            'FROM "E-commerce Customer Behavior - Sheet1")'
+        )
 
-    count_min = sqlite_lib.run_query_select(
-        'SELECT "Customer ID", "Items Purchased" '
-        'FROM "E-commerce Customer Behavior - Sheet1" '
-        'WHERE "Items Purchased" = (SELECT MIN("Items Purchased") '
-        'FROM "E-commerce Customer Behavior - Sheet1")'
-    )
+        count_min = sqlite_lib.run_query_select(
+            'SELECT "Customer ID", "Items Purchased" '
+            'FROM "E-commerce Customer Behavior - Sheet1" '
+            'WHERE "Items Purchased" = (SELECT MIN("Items Purchased") '
+            'FROM "E-commerce Customer Behavior - Sheet1")'
+        )
 
-    sqlite_lib.close()
-
-    count_max_output = [info for info in count_max]
-    count_min_output = [info for info in count_min]
+        count_max_output = [info for info in count_max]
+        count_min_output = [info for info in count_min]
     return count_max_output, count_min_output
 
 if __name__ == "__main__":
@@ -160,4 +157,12 @@ if __name__ == "__main__":
     for customer, items in min_results:
         print(f"Customer no. {customer} bought the smallest number of products - {items}")
 
+def members_count (membership:str):
+    with db_connection():
+        count = sqlite_lib.run_query_select(f'SELECT COUNT ("Customer ID")'
+                                            f'FROM "E-commerce Customer Behavior - Sheet1"'
+                                            f'WHERE "Membership Type" = "{membership}" ')
+    return count[0][0]
+
+print(members_count('Gold'))
 
